8eb10f8720b1f8e33298d2295934c01737b4fe54
irohanrajput <itusharsingh27@gmail.com>
2023-09-14 03:56:38 +0530

    now tempr showing in celsius

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -4,15 +4,18 @@ from django.conf import settings
 
 def get_info(request):
     api_key = settings.WEATHER_API_KEY
-    city = 'India'
+    city = 'delhi, in'
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
 
     response = requests.get(url)
     data = response.json()  
+    temp_in_kelvin = data['main']['temp']
+    temp_in_celsius = round(temp_in_kelvin -273.15, 3)
 
     context = {
-        'temperature': data['main']['temp'],
+        'temperature': temp_in_celsius,
         'description': data['weather'][0]['description'],
+        'city': city
     }
     
     return render(request, 'index.html', context)
